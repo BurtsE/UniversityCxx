f433cf209c471135295e7747482f54aae8e81919
beusel <burcev-egor@mail.ru>
2025-06-30 12:34:10 +0500

    service comments

--- service/university/include/university.hpp
+++ service/university/include/university.hpp
@@ -3,7 +3,14 @@
 #include <in_memory_repository.hpp>
 #include <builder.hpp>
 
-class University {
+
+/**
+ * @brief Service class for the app
+ * 
+ * Handles work with storage, generates student's codes
+ * 
+ */
+class University { 
 
 private:
 
@@ -16,14 +23,49 @@ private:
 public:
     University(InMemoryTable& storage): storage_(storage){};
 
+    /**
+     * @brief Creates Junior student in the database
+     * 
+     * @param _name 
+     * @param _groupIndex 
+     * @param _departmentNumber 
+     * @return ITable::student_code UUID of a student
+     */
     ITable::student_code admission(std::string _name, GroupIndex _groupIndex, Department _departmentNumber);
 
+    /**
+     * @brief creates another student with promoted class
+     * 
+     * throws error if student could not be graduated
+     * 
+     * @param code 
+     */
     void promote_student(ITable::student_code code);
 
+    /**
+     * @brief deletes student from database
+     * 
+     * @param code 
+     */
     void expel_student(ITable::student_code code);
-
+    
+    /**
+     * @brief Get the group grades
+     * 
+     * Runs in multiple threads (one for each student group type) 
+     * 
+     * @param _departmentNumber 
+     * @return std::vector<std::pair<GroupIndex, double>> 
+     */
     std::vector<std::pair<GroupIndex, double>> get_group_grades(Department _departmentNumber);
 
+    /**
+     * @brief Set the student session grade for subject
+     * 
+     * @param code 
+     * @param subject 
+     * @param grade 
+     */
     void set_student_session_grade(ITable::student_code code, size_t subject, unsigned grade);
 
 };
